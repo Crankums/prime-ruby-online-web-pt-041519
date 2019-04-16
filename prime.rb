fc673cdec05ff313293a19dedfb26ed504bc0d3d
Craig Donavin <crdonavin@gmail.com>
2019-04-16 21:00:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(num)
   if num>0 | num == 0 | num == 1
     return false
   else
-    (2..num-1)to_a.all? do |integer|
+    (2..num-1).to_a.all? do |integer|
       num % integer != 0
     end
   end
